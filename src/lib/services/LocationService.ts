f1631bcc4ecefcd4dd248311ad63b8bc290161f2
Prithviraj Mazumder <prithirajmajumder8@gmail.com>
2024-05-23 00:09:58 +0530

    feat: adds support for saved locations

---
+++ src/lib/services/LocationService.ts
@@ -0,0 +1,21 @@
+const LOCATIONS_KEY_KEY = 'SAVED_LOCATIONS';
+
+export const saveLocation = (location: string) => {
+	const savedLocations = getSavedLocations();
+	if (
+		savedLocations.find((savedLocation) => location.toUpperCase() === savedLocation.toUpperCase())
+	) {
+		return;
+	}
+	localStorage.setItem(LOCATIONS_KEY_KEY, JSON.stringify({ locations: [...savedLocations, location] }));
+};
+
+export const getSavedLocations = (): string[] => {
+	const locations = localStorage.getItem(LOCATIONS_KEY_KEY);
+	if (!locations?.length) {
+		return [];
+	}
+
+	console.log(locations);
+	return JSON.parse(locations).locations as string[];
+};
