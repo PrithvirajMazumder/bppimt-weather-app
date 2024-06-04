3c6affef8d7bfd6151ab43c0da599327e5048eaa
Prithviraj Mazumder <prithirajmajumder8@gmail.com>
2024-06-04 22:54:36 +0530

    style: adds sun duration

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,15 +3,17 @@
 	import { getSavedLocations, saveLocation } from '$lib/services/LocationService';
 	import clearNightImage from '$lib/images/Dark Cloudy Sky Photo.jpg';
 	import { AQUSDefraIndex } from '$lib/constants/AQUSDefraIndex';
+	import { format } from 'date-fns';
 
 
-	const API_KEY: string = 'fa026116b11a4b7ca30194511242704';
+	const API_KEY: string = 'e6c675e37cb846b1b40163308240406';
 	const BASE_URL: string = 'https://api.weatherapi.com/v1';
 	const ENDPOINT: string = 'current.json';
 	const FORECAST_ENDPOINT: string = 'forecast.json';
 	const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 	let weatherInfo: any = null;
+	let astroInfo: any = null;
 	let forecast: any = null;
 	let isLoading: boolean;
 	let hasError: unknown;
@@ -26,13 +28,17 @@
 			const respWeatherInfo = await (
 				await fetch(`${BASE_URL}/${ENDPOINT}?q=${query}&key=${API_KEY}&&aqi=yes`)
 			).json();
+			const astroInfoResp = await (
+				await fetch(`${BASE_URL}/astronomy.json?q=${query}&dt=${format(new Date(), 'yyyy-MM-dd')}&key=${API_KEY}&&aqi=yes`)
+			).json();
+
 			//calling api of forecast weather info
 			const respForecast = await (
 				await fetch(`${BASE_URL}/${FORECAST_ENDPOINT}?q=${query}&days=5&key=${API_KEY}&&aqi=yes`)
 			).json();
 			weatherInfo = respWeatherInfo;
 			forecast = respForecast.forecast;
-
+			astroInfo = astroInfoResp;
 			if (respWeatherInfo?.error) {
 				hasError = respWeatherInfo?.error;
 				return;
@@ -41,6 +47,7 @@
 			currentLocation = query;
 		} catch (error) {
 			alert('Something went wrong!');
+			console.error(error);
 		} finally {
 			isLoading = false;
 		}
@@ -60,11 +67,17 @@
 			);
 		} catch (error) {
 			alert('Something went wrong');
+			console.error(error);
 		}
 	};
 
 	onMount(() => {
 		savedLocations = getSavedLocations() ?? [];
+		window?.navigator.geolocation.watchPosition((position) => {
+			console.log('hello success geo location log: ', position);
+		}, (positionError) => {
+			console.log('positionError: ', positionError);
+		});
 		if (savedLocations?.length) {
 			currentLocation = savedLocations[0];
 			void fetchWeatherInfo(savedLocations[0]);
@@ -84,8 +97,8 @@
 			<div class="navbar bg-glass-morphic px-6">
 				<h1 class="text-xl">BPPIMT Weather</h1>
 			</div>
-			<div class="h-[calc(100%-5.5rem)] overflow-y-scroll flex gap-6 w-full items-start pt-6">
-				<div class="w-full">
+			<div class="h-[calc(100svh-5.5rem)] flex gap-6 w-full items-start py-6">
+				<div class="w-[calc(100svw-45rem)] overflow-y-scroll h-full">
 					<div class="flex gap-4 items-end -mb-8">
 						<h1 class="text-4xl text-gray-300/70 break-all"><span class="text-sm">Current condition ☁ at</span>
 							<br>{weatherInfo?.location?.name}, {weatherInfo?.location?.region}</h1>
@@ -109,12 +122,8 @@
 						</button>
 
 					</div>
-					<h1 class="text-[10rem] break-all">{weatherInfo?.current?.condition?.text}</h1>
+					<h1 class="text-[6rem] break-words my-16 leading-[1]">{weatherInfo?.current?.condition?.text}</h1>
 					<div class="flex gap-4">
-						<div class="bg-glass-morphic p-4 w-full">
-							<p class="️text-sm text-gray-300/30 mb-4">🌡 Feels like</p>
-							<h3 class="text-5xl">{weatherInfo?.current?.feelslike_c}°c</h3>
-						</div>
 						<div class="bg-glass-morphic p-4 w-full">
 							<p class="️text-sm text-gray-300/30 mb-4">💨 Wind</p>
 							<h3 class="text-5xl">{weatherInfo?.current?.wind_kph}</h3>
@@ -182,12 +191,101 @@
 							</div>
 						{/if}
 					</div>
+					<div class="bg-glass-morphic w-full p-6 mt-6">
+						<p class="text-lg text-gray-300/50">Hourly forecast</p>
+						<div class="divider my-0" />
+						<div
+							class="flex items-center w-full overflow-x-scroll mt-2"
+						>
+							{#if hourlyForecast}
+								{#each hourlyForecast as hourly}
+									<div class="flex flex-col min-w-[5rem] items-center">
+										<p class="text-gray-300/60">
+											{format(new Date(hourly?.forecast?.forecastday[0]?.hour[0]?.time ?? ''), 'h aa')}
+										</p>
+										<img
+											class="h-[2rem] w-[2rem]"
+											src={hourly?.forecast?.forecastday[0]?.hour[0]?.condition?.icon}
+											alt="Weather condition icon"
+										/>
+										<p
+											class="font-semibold text-gray-300/60 text-lg">{Math.trunc(hourly?.forecast?.forecastday[0]?.hour[0]?.temp_c ?? 0)}{'°'}</p>
+									</div>
+								{/each}
+
+							{/if}
+
+						</div>
+					</div>
+					<div class="mt-6 bg-glass-morphic p-6 h-max w-max">
+						<iframe width="650" height="300" class="rounded-lg"
+										src="https://embed.windy.com/embed.html?type=map&location=coordinates&metricRain=default&metricTemp=default&metricWind=default&zoom=100&overlay=radar&product=radar&level=surface&lat={weatherInfo?.location?.lat}&lon={weatherInfo?.location?.lon}&pressure=true&message=true"
+										frameborder="0" />
+
+					</div>
 				</div>
 				<div
-					class="w-96 h-full bg-glass-morphic p-6 text-gray-300 flex flex-col items-start gap-4">
-					<h1 class="text-8xl font-semibold">
+					class="w-96 h-full overflow-y-scroll bg-glass-morphic p-6 text-gray-300 flex flex-col items-start gap-4">
+					<h1 class="text-8xl font-semibold leading-[0] mt-12 mb-8">
 						{weatherInfo?.current?.temp_c}°c
 					</h1>
+					<p class="text-lg">Feels like {weatherInfo?.current?.feelslike_c}°c</p>
+					<span class="bg-glass-morphic !rounded-[50px] w-full px-3 py-1 flex items-center">Wind 💨 direction <span
+						class="font-semibold mx-1">{weatherInfo?.current?.wind_dir}</span></span>
+					<div class="flex flex-col w-full gap-4">
+						{#if forecast}
+							{#each forecast.forecastday as day}
+								<div class="flex items-center w-full gap-2">
+									<div class="h-12 w-12 flex flex-col justify-center items-center bg-glass-morphic">
+										<img
+											class="h-8 w-8"
+											src={day?.day?.condition?.icon}
+											alt={day?.day?.condition?.text}
+										/>
+									</div>
+									<div>
+										<h3 class="text-md text-gray-300">
+											{daysOfWeek[new Date(day.date).getDay()]}
+										</h3>
+										<h3 class="text-xs text-gray-300/50">{day?.day?.condition?.text}</h3>
+
+									</div>
+
+									<h3 class="text-md font-semibold text-gray-300 ml-auto">{Math.trunc(day?.day?.avgtemp_c ?? 0)}
+										&deg;</h3>
+								</div>
+							{/each}
+						{/if}
+					</div>
+					<div class=" mt-4 w-full bg-glass-morphic p-4">
+						<div class="flex flex-col items-center w-full px-4">
+							<p class="text-gray-300/40 text-p mb-2">Sun duration</p>
+							<div class="bg-gray-300/30 h-1 rounded-full w-full relative mb-4" />
+						</div>
+						<div class="flex w-full items-center  justify-between gap-2">
+							<div class="flex flex-col items-center">
+								<svg class="w-4 h-4 text-gray-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+										 width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+									<path fill-rule="evenodd"
+												d="M13 3a1 1 0 1 0-2 0v2a1 1 0 1 0 2 0V3ZM6.343 4.929A1 1 0 0 0 4.93 6.343l1.414 1.414a1 1 0 0 0 1.414-1.414L6.343 4.929Zm12.728 1.414a1 1 0 0 0-1.414-1.414l-1.414 1.414a1 1 0 0 0 1.414 1.414l1.414-1.414ZM12 7a5 5 0 1 0 0 10 5 5 0 0 0 0-10Zm-9 4a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2H3Zm16 0a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2h-2ZM7.757 17.657a1 1 0 1 0-1.414-1.414l-1.414 1.414a1 1 0 1 0 1.414 1.414l1.414-1.414Zm9.9-1.414a1 1 0 0 0-1.414 1.414l1.414 1.414a1 1 0 0 0 1.414-1.414l-1.414-1.414ZM13 19a1 1 0 1 0-2 0v2a1 1 0 1 0 2 0v-2Z"
+												clip-rule="evenodd" />
+								</svg>
+								<p class="text-gray-300">{astroInfo?.astronomy?.astro?.sunrise}</p>
+							</div>
+
+							{#if astroInfo}
+								<div class="flex flex-col items-center">
+									<svg class="w-4 h-4 text-gray-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
+											 height="24" fill="none" viewBox="0 0 24 24">
+										<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+													d="M12 5V3m0 18v-2M7.05 7.05 5.636 5.636m12.728 12.728L16.95 16.95M5 12H3m18 0h-2M7.05 16.95l-1.414 1.414M18.364 5.636 16.95 7.05M16 12a4 4 0 1 1-8 0 4 4 0 0 1 8 0Z" />
+									</svg>
+									<p class="text-gray-300">{astroInfo?.astronomy?.astro?.sunset}</p>
+								</div>
+							{/if}
+						</div>
+					</div>
+
 				</div>
 			</div>
 		</div>
